a6e58047ab8db16966e0e962ff3e11c717450753
NahinNilav <ehsannilav@gmail.com>
2024-05-18 03:08:26 +0600

    Some changes

--- src/main/java/org/example/restaurant_management_system/Controllers/FXMLDocumentController.java
+++ src/main/java/org/example/restaurant_management_system/Controllers/FXMLDocumentController.java
@@ -1,8 +1,4 @@
-package org.example.restaurant_management_system.Controllers;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package org.example.restaurant_management_system.Controllers;
 
 
 import java.net.URL;
