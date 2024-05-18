42c193a086b5d443a612d33128803cfe6ade3665
NahinNilav <ehsannilav@gmail.com>
2024-05-18 14:02:30 +0600

    Database class final modification

--- src/main/java/org/example/restaurant_management_system/Model/Database.java
+++ src/main/java/org/example/restaurant_management_system/Model/Database.java
@@ -6,30 +6,6 @@ import java.sql.SQLException;
 
 public class Database {
 
-    // DO NOT EDIT ANYTHING
-    // TABLE EMPLOYEE HARDCODED FOR LOGIN SIGNUP. RESOLVE THIS
-    // INCLUDE JAR IN LIBRARIES
-    // Import java.sql.Connection for JDBC database connections
-    // You're using Class.forName("com.mysql.jdbc.Driver") to load the MySQL driver
-    // However, in recent versions of MySQL Connector/J, the driver class has
-    // changed to com.mysql.cj.jdbc.Driver
-
-    /*
-    public static Connection connectDB() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3307/restaurant", "root", "");
-            return connect;
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-}
-*/
-
-
     /*
     private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
@@ -55,27 +31,27 @@ public class Database {
 
      */
 
-    private static final String URL = "jdbc:mysql://localhost:3307/restaurant";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    //  Singleton pattern
+    private static final String url = "jdbc:mysql://localhost:3307/restaurant";
+    private static final String username = "root";
+    private static final String pwd = "";
     private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
     private static Connection connection;
 
-    // Private constructor to prevent instantiation from outside
     private Database() {
-        // Initialize the connection
         try {
             Class.forName(DRIVER_CLASS);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = DriverManager.getConnection(url, username, pwd);
         }
 
         catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Database connection problem occured");
             e.printStackTrace();
         }
     }
 
-    // Static method to get the singleton instance
+
     public static synchronized Connection connectDB() {
         if (connection == null) {
             new Database();
